7eb78ac4e1d2ee462ba63b74f0b429c4b23efbb1
Jaypad07 <jaypad07@gmail.com>
2023-04-28 09:20:13 -0700

    Added packages

---
+++ src/test/java/com/example/prescriptionapi/PrescriptionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prescriptionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrescriptionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
