3e0df05679060d9f30cca77635646064ec0668f0
Jaypad07 <jaypad07@gmail.com>
2023-04-30 13:57:30 -0700

    fixed getPatient to check for null

--- src/main/java/com/example/prescriptionapi/controller/PatientController.java
+++ src/main/java/com/example/prescriptionapi/controller/PatientController.java
@@ -1,6 +1,7 @@
 package com.example.prescriptionapi.controller;
 
 import com.example.prescriptionapi.exception.InformationExistException;
+import com.example.prescriptionapi.exception.InformationNotFoundException;
 import com.example.prescriptionapi.model.Patient;
 import com.example.prescriptionapi.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,8 +39,8 @@ public class PatientController {
     @GetMapping(path = "/patient/{patientId}")
     public Patient getPatient(@PathVariable Long patientId)    {
         Optional<Patient> patient = patientRepository.findById(patientId);
-        if (patient.isPresent()) {
-            throw new InformationExistException("Patient with id " + patientId + " already exists");
+        if (patient.isEmpty()) {
+            throw new InformationNotFoundException("Patient with id " + patientId + " does not exist.");
         }else return patient.get();
     }
 
